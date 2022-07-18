69369ca8ed26d43da14acc2364c81bce747794e0
Isaac Marovitz <42140194+IsaacMarovitz@users.noreply.github.com>
2022-07-18 19:55:05 +0100

    Initial Commit

---
+++ SwiftUI Test/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI Test
+//
+//  Created by Isaac Marovitz on 18/07/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI Test/SwiftUI_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_TestApp.swift
+//  SwiftUI Test
+//
+//  Created by Isaac Marovitz on 18/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
